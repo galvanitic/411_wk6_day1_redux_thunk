95a8afce116663662439d8d7e9f87bab3293b22e
galvanitic <develop@rjgalvan.com>
2020-02-27 20:54:25 -0600

    Added table

--- src/components/Import.js
+++ src/components/Import.js
@@ -4,16 +4,43 @@ import Button from '@material-ui/core/Button';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
+// import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+
+// function createData(id, make, actions) {
+//     return {
+//         id, make, actions
+//     }
+// }
 
 const Import = (props) => {
     // fill out this component
 
-    return (
-        <Button variant="contained">Import</Button>
-    )
+  return (
+    <div className='table'>
+      <Button variant="contained">Import</Button>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell></TableCell>
+            <TableCell align="left">ID</TableCell>
+            <TableCell align="left">Make</TableCell>
+            <TableCell align="left">Actions</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {props.makes.map(e => {
+            <TableRow>
+              <TableCell></TableCell>
+              <TableCell></TableCell>
+              <TableCell></TableCell>
+            </TableRow>
+          })}
+        </TableBody>
+      </Table>
+    </div>
+  )
 }
 
 export default Import
--- src/containers/Import.js
+++ src/containers/Import.js
@@ -1,1 +1,10 @@
-// fill out this container
+import { connect } from 'react-redux'
+import Import from '../components/Import'
+
+const mapStateToProps = (state) => {
+    return {
+        makes: state.makes
+    }
+}
+
+export default connect(mapStateToProps)(Import)
